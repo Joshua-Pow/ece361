c3403ed2bb971ab44c2baa59381012b29596eb92
Joshua Pow <joshua.pow@mail.utoronto.ca>
2022-01-13 19:30:41 -0500

    Filled out the server.c using Beejs guide to network programming on pg 37

--- deliver.c
+++ deliver.c
@@ -1,5 +1,16 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+
+//deliver 128.100.13.65 3550
 int main(int argc, char const *argv[])
 {
-    /* code */
+    printf("IP: %s\n", argv[1]);
+    printf("Port: %s\n", argv[2]);
+
+    struct sockaddr_in socketAddr;
+    char str[INET_ADDRSTRLEN];
+
+    inet_pton(AF_INET, argv[1], &(socketAddr.sin_addr));
+
     return 0;
 }
--- server.c
+++ server.c
@@ -1,5 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MAXBUFLEN 100
+
+void* get_in_addr(struct sockaddr *sa);
+
+//Source: "Beejs Guid To Network Programming pg.37"
 int main(int argc, char const *argv[])
 {
-    /* code */
+    const char* port = argv[1];
+    printf("Port: %s\n", port);
+
+    struct addrinfo hints; //Criteria to be returned from get_addr
+    struct addrinfo *servinfo;
+    struct addrinfo *p;
+
+    struct sockaddr_storage their_addr;
+
+    char buf[MAXBUFLEN];
+	socklen_t addr_len;
+	char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; //IPv4
+    hints.ai_socktype = SOCK_DGRAM; //UDP (SOCK_STREAM is TCP)
+    hints.ai_flags = AI_PASSIVE; //accepts anything on the network?
+
+    int rv; //return value
+    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    int sockfd; //socket file descriptor
+    for(p = servinfo; p != NULL; p = p->ai_next) { //Might not need to loop through all packets
+		if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			perror("listener: socket");
+			continue;
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("listener: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "listener: failed to bind socket\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo);
+
+	printf("listener: waiting to recvfrom...\n");
+
+    int numbytes;
+	addr_len = sizeof their_addr;
+	if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+		(struct sockaddr *)&their_addr, &addr_len)) == -1) {
+		perror("recvfrom");
+		exit(1);
+	}
+
+	printf("listener: got packet from %s\n",
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s));
+	printf("listener: packet is %d bytes long\n", numbytes);
+	buf[numbytes] = '\0';
+	printf("listener: packet contains \"%s\"\n", buf);
+
+	close(sockfd);
+
+	return 0;
+
+
     return 0;
+}
+
+//Get sockaddr, IPv4 or IPv6
+void* get_in_addr(struct sockaddr *sa){
+    //AF_INET is IPv4
+    //AF_INET6 is IPv6
+    if (sa->sa_family == AF_INET) {
+        //sockaddr_in is the same size as sockaddr
+        //source: https://www.gta.ufrj.br/ensino/eel878/sockets/sockaddr_inman.html
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
