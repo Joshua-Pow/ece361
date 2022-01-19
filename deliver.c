4606960a9e33dd5c45b6c9a49581a06f159200c5
Tashan Maniyalaghan <tashan.maniyalaghan@mail.utoronto.ca>
2022-01-18 21:32:55 -0500

    Initial code for client

--- deliver.c
+++ deliver.c
@@ -1,16 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
 //deliver 128.100.13.65 3550
 int main(int argc, char const *argv[])
 {
-    printf("IP: %s\n", argv[1]);
-    printf("Port: %s\n", argv[2]);
+    int sockfd;
+    struct addrinfo hints;
+    struct addrinfo* servinfo;
+    struct addrinfo* p;
+    int rv;
+    int numbytes;
 
-    struct sockaddr_in socketAddr;
-    char str[INET_ADDRSTRLEN];
+    char* message; //temp
 
-    inet_pton(AF_INET, argv[1], &(socketAddr.sin_addr));
+    if (argc != 3) {
+        fprintf(stderr, "usage: Missing/too many arguments\n");
+        exit(1);
+    }
+
+    const char* serverAddress = argv[1];
+    const char* serverPort = argv[2];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET6; //IPv4
+    hints.ai_socktype = SOCK_DGRAM; // UDP
+
+    if ((rv = getaddrinfo(serverAddress, serverPort, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to create socket\n");
+        return 2;
+    }
+
+    if ((numbytes = sendto(sockfd, temp, strlen(temp), 0, p->ai_addr, p->ai_addrlen)) == -1) {
+        perror("client: sendto");
+        exit(1);
+    }
+
+    freeaddrinfo(servinfo);
+
+    printf("client: sent %d bytes to %s\n", numbytes, temp)
 
     return 0;
 }
